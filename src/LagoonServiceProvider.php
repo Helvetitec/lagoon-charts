601f7c8b2ab408bf1670f90c8a7702c30e9d6414
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-04 17:32:06 -0300

    Added config

--- src/LagoonServiceProvider.php
+++ src/LagoonServiceProvider.php
@@ -11,7 +11,7 @@ class LagoonServiceProvider extends ServiceProvider
 {
   public function register()
   {
-    //
+    $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'lagoon');
   }
 
   public function boot()
@@ -19,5 +19,12 @@ class LagoonServiceProvider extends ServiceProvider
     $this->loadViewsFrom(__DIR__.'/../resources/views', 'lagoon');
     Livewire::component('lagoon-line-chart', LineChart::class);
     Livewire::component('lagoon-pie-chart', PieChart::class);
+
+    if ($this->app->runningInConsole())
+    {
+      $this->publishes([
+        __DIR__.'/../config/config.php' => config_path('lagoon.php'),
+      ], 'config');
+    }
   }
 }
