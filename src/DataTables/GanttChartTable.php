f49c3c75a437c697f5a7f30ea0786b7d5575ef08
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-06 13:10:37 -0300

    Update GanttChartTable.php

--- src/DataTables/GanttChartTable.php
+++ src/DataTables/GanttChartTable.php
@@ -8,23 +8,29 @@ class GanttChartTable{
     private $data = [];
 
     public function addTask(string $id, string $name, Carbon $start, Carbon $end, int $duration, int $completion, ?string $dependencies){
-        array_push($this->data, [ "c" =>
-            [
-                "v" => $id,
-                "v" => $name,
-                "v" => null,
-                "v" => "Date(".$start->year.','.($start->month - 1).','.$start->day.")",
-                "v" => "Date(".$end->year.','.($end->month - 1).','.$end->day.")",
-                "v" => $duration * 24 * 60 * 60 * 1000,
-                "v" => $completion,
-                "v" => $dependencies
-            ]
+        array_push($this->data, [
+            $id,
+            $name,
+            null,
+            "Date(".$start->year.','.($start->month - 1).','.$start->day.")",
+            "Date(".$end->year.','.($end->month - 1).','.$end->day.")",
+            $duration * 24 * 60 * 60 * 1000,
+            $completion,
+            $dependencies
         ]);
     }
 
     public function __toString()
     {
-        return json_encode($this->data);
+        $str = "[";
+
+        foreach($this->data as $item){
+            $str .= "{c: [{v: '".$item[0]."', {v: '".$item[1]."'}, {v: '".$item[2]."'}, {v: '".$item[3]."'}, {v: '".$item[4]."'}, {v: '".$item[5]."'}, {v: '".$item[6]."'}, {v: '".$item[7]."'}]}\r\n";
+        }
+
+        $str .= "]";
+
+        return $str;
     }
 
     public function toArray():array{
