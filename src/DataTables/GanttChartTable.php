7b0afadf159fabf273019e24f372f8e3ac5602c7
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-06 13:03:42 -0300

    Update GanttChartTable.php

--- src/DataTables/GanttChartTable.php
+++ src/DataTables/GanttChartTable.php
@@ -3,21 +3,22 @@
 namespace Helvetiapps\LagoonCharts\DataTables;
 
 use Carbon\Carbon;
-use Illuminate\Support\Facades\Date;
 
 class GanttChartTable{
     private $data = [];
 
     public function addTask(string $id, string $name, Carbon $start, Carbon $end, int $duration, int $completion, ?string $dependencies){
-        array_push($this->data, [
-            $id,
-            $name,
-            null,
-            new Date($start->year,($start->month - 1),$start->day,0,0,0,0),
-            new Date($end->year,($end->month - 1),$end->day,0,0,0,0),
-            $duration * 24 * 60 * 60 * 1000,
-            $completion,
-            $dependencies
+        array_push($this->data, [ "c" =>
+            [
+                "v" => $id,
+                "v" => $name,
+                "v" => null,
+                "v" => "Date(".$start->year.','.($start->month - 1).','.$start->day.")",
+                "v" => "Date(".$end->year.','.($end->month - 1).','.$end->day.")",
+                "v" => $duration * 24 * 60 * 60 * 1000,
+                "v" => $completion,
+                "v" => $dependencies
+            ]
         ]);
     }
 
