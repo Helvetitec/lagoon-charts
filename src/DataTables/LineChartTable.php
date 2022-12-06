f659b712ce31fce1b1edceb1e10e9dff8cadad71
Roman Wanner <60240491+daredloco@users.noreply.github.com>
2022-12-06 09:56:47 -0300

    Added headers to LineChartTable

--- src/DataTables/LineChartTable.php
+++ src/DataTables/LineChartTable.php
@@ -9,14 +9,15 @@ class LineChartTable{
     private $data = [];
     private $colCount;
 
-    public function __construct(int $colCount, $rows = [])
+    public function __construct(array $headers, array $rows = [])
     {
-        $this->colCount = $colCount;
+        $this->colCount = count($headers);
         $rowCount = 0;
+        $this->data = [$headers];
         foreach($rows as $row){
             $rowCount++;
-            if(count($row) != $colCount){
-                throw new Exception("Row ".$rowCount." has ".count($row)." columns but needs to have ".$colCount."!");
+            if(count($row) != $this->colCount){
+                throw new Exception("Row ".$rowCount." has ".count($row)." columns but needs to have ".$this->colCount."!");
             }
             array_push($this->data, $row);
         }
